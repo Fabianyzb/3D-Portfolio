56e986132e1bb249c0f00d27e5ef68dae8cf1f9b
Fabian Yanez <fabian.yzb@gmail.com>
2024-02-03 16:00:30 -0300

    SectionWrapper y Hero section listos pero falta pointer al scroll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,9 +22,12 @@ const Hero = () => {
             </div>
             <ComputersCanvas />
 
-            <div className='absoute xs:bottom-10 bottom-32 w-full flex  justify-center items-center'>
+
+            {/* "Gif" de la pildora para hacer scrolldown */}
+            <div className='absoute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
                 <a href="#about">
-                    <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+                    <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary 
+                    flex justify-center items-start p-2'>
                         {/* Aqui se ocupa el framer-motion */}
                         <motion.div
                             animate={{
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -4,11 +4,22 @@ import { styles } from '../styles'
 import { staggerContainer } from '../utils/motion'
 
 /* Una funcion retornando otra funcion */
+/* El SectionWrapper se aplicara a diferentes secciones para que hagan el 
+efecto de "aparecer que se configura en el motion.section"  */
 const SectionWrapper = (Component, idName) =>
     function HOC() {
         return (
-            <motion.section>
-
+            <motion.section
+                variants={staggerContainer()}//animara laa seccion
+                initial='hidden'
+                whileInView='show'
+                viewport={{ once: true, amount: 0.25 }}
+                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+            >
+                {/* se agrega funcionalidad para el scrolldown con un empty space(&nbsp;) */}
+                <span className='hash-span' id={idName}>
+                    &nbsp;
+                </span>
                 <Component />
             </motion.section>
         )
