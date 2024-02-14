b17771a2ae3d1e109ca622554bf28e8f5851e7b7
Fabian Yanez <fabian.yzb@gmail.com>
2024-02-13 23:02:16 -0300

    haciendo seccion works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -19,7 +19,11 @@ const Works = () => {
       <div className='w-full flex'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading=[30px]'>
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading=[30px]'
+        >
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Ipsam quaerat aut in tempore amet voluptate ipsum maxime.
+          (Escribir algo sobre mis projects)
         </motion.p>
       </div>
     </>
