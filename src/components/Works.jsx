d8d5adc2f30a381165fa5939b0ac1921af31bfad
Fabian Yanez <fabian.yzb@gmail.com>
2024-02-13 21:44:50 -0300

    haciendo seccion works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,7 +8,19 @@ import { fadeIn, TextVariant } from '../utils/motion'
 const Works = () => {
   return (
     <>
+      <motion.div variants={textVariant()}> {/* Esto hara que las etiquetas p sean animadas */}
+        <p className={styles.sectionSubText
+        }>My work</p>
+        <h2 className={styles.sectionHeadText}>
+          Projects.
+        </h2>
+      </motion.div>
 
+      <div className='w-full flex'>
+        <motion.p>
+
+        </motion.p>
+      </div>
     </>
   )
 }
