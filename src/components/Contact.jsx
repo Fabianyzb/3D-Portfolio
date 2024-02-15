8a84947d4a48e4b310be8e346fc4e31ae19ebea8
Fabian Yanez <fabian.yzb@gmail.com>
2024-02-15 20:10:54 -0300

    haciendo Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,20 @@ import { useState, useRef } from 'react'
 import { motion } from 'framer-motion'
 import emailjs from '@emailjs/browser'
 
+import { styles } from '../styles'
+import { EarthCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { slideIn } from '../utils/motion'
+
 const Contact = () => {
+  /* especificar un campo vacio useRef */
+  const formRef = useRef();
+  /* especificar el campo useState */
+  const [form, setform] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
   return (
     <div>Contact</div>
   )
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,6 @@
 import Tilt from 'react-parallax-tilt'
 import { motion } from 'framer-motion'
+
 import { styles } from '../styles'
 import { github } from '../assets'
 import { SectionWrapper } from '../hoc'
