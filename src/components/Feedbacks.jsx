1b790491f808b2cd09a268c54a2edfa172645bf1
Fabian Yanez <fabian.yzb@gmail.com>
2024-02-15 22:17:24 -0300

    haciendo Contact y EarthCanvas

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,8 +1,11 @@
+/* Esta seccion se pospone */
+
+
 /* import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { SectionWrapper } from '../hoc'
 import { fadeIn, textVariant } from '../utils/motion'
-import { testimonials } from '../constants'
+import { testimonials } from '../constants' */
 
 const Feedbacks = () => {
   return (
@@ -10,4 +13,4 @@ const Feedbacks = () => {
   )
 }
 
-export default Feedbacks */
+export default Feedbacks
