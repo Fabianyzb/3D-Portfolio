73877e90aa5dfe5c2acfbccdadfcb7ea19bd28cd
Fabian Yanez <fabian.yzb@gmail.com>
2024-02-17 16:21:34 -0300

    Capsula seccion Hero arreglada

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+
 import { styles } from "../styles"
 import { ComputersCanvas } from './canvas'
 
@@ -22,13 +23,11 @@ const Hero = () => {
             </div>
             <ComputersCanvas />
 
-
-            {/* "Gif" de la pildora para hacer scrolldown */}
-            <div className='absoute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-                <a href="#about">
+            {/* Capsula para hacer scrolldown con enlace de desplazamiento a seccion #about */}
+            <div className='absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center'>
+                <a href='#about'>
                     <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary 
                     flex justify-center items-start p-2'>
-                        {/* Aqui se ocupa el framer-motion */}
                         <motion.div
                             animate={{
                                 y: [0, 24, 0]
@@ -36,7 +35,7 @@ const Hero = () => {
                             transition={{
                                 duration: 1.5,
                                 repeat: Infinity,
-                                repeatType: 'loop'
+                                repeatType: 'loop',
                             }}
                             className='w-3 h-3 rounded-full bg-secondary mb-1' />
                     </div>
