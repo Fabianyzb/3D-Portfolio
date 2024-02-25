71372ea9c8be74f7eccae226368a65da9dfafdad
Fabian Yanez <fabian.yzb@gmail.com>
2024-02-24 22:28:15 -0300

    feat

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -76,8 +76,8 @@ const Contact = () => {
         variants={slideIn('left', 'tween', 0.2, 1)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl' /* flex-[0.75] es 3/4 de la pantalla */
       >
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <p className={styles.sectionSubText}>Háblame</p>
+        <h3 className={styles.sectionHeadText}>Contacto.</h3>
 
         <form
           ref={formRef}
